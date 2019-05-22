611f69f0cf5cc40365a880032a20965846e9a294
Ashish Rao <ashishrao2598@gmail.com>
2019-05-22 11:57:58 +0530

    add devise and devise-bootstrap gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_135519) do
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
+ActiveRecord::Schema.define(version: 2019_05_22_060541) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -51,13 +30,6 @@ ActiveRecord::Schema.define(version: 2019_05_19_135519) do
     t.string "name"
   end
 
-  create_table "libraries", force: :cascade do |t|
-    t.integer "book_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "publishers", force: :cascade do |t|
     t.string "name"
   end
@@ -70,7 +42,6 @@ ActiveRecord::Schema.define(version: 2019_05_19_135519) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
